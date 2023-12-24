2cb1f71ffc23495af3c6051b9b0ffc7a78ecb557
Julian <48083546+JulianStoev@users.noreply.github.com>
2023-12-24 19:08:52 +0200

    update headers

--- utils/FileUpload/FileUpload.interface.ts
+++ utils/FileUpload/FileUpload.interface.ts
@@ -1,13 +1,14 @@
 export interface FileUploadDataInterface {
-    id?: number | string;
     chunkSize?: number;
     instantUpload?: boolean;
     url: string;
-    headers?: {[key: string]: string};
+    headers?: {
+        [key: string]: string
+    };
     onStart?: (arg0: ProgressEvent<EventTarget>) => void;
     onProgress?: (arg0: number) => void;
     onAbort?: (arg0: ProgressEvent<EventTarget>) => void;
     onError?: (arg0: ProgressEvent<EventTarget>) => void;
-    onDone?: () => void;
+    onDone?: (arg0: any) => void;
     onChange?: (files: FormData[]) => void;
 }
