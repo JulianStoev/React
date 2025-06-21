37ebbbc1f9d7c8f7c7b60a67a939f4fc9760058c
Julian <48083546+JulianStoev@users.noreply.github.com>
2025-06-21 21:47:21 +0300

    Update cache.interface.ts

--- utils/Cache/cache.interface.ts
+++ utils/Cache/cache.interface.ts
@@ -1,11 +1,43 @@
-export interface CachedDataInterface {
+export interface cacheInterface {
+    set: (cacheToSet: cacheToSetInterface, maxLimit?: number, timeInMinutes?: number) => void;
+    get: (cacheName: string) => any;
+    getPartial: (partialKey: string | string[], index?: number) => cachedDataInterface[];
+    del: (cacheName: string) => boolean;
+    delMultiple: (cacheArr: cachedDataInterface[]) => void;
+    flush: () => void;
+}
+
+export interface cacheDataInterface {
+    name: string; 
+    reorder?: boolean;
+}
+
+export interface cacheObjInterface {
+    [cacheName: string]: cachedDataInterface[];
+}
+
+export interface cachedDataInterface {
     key: string;
     expirationDate: number;
     val: any;
 }
 
-export interface SetCacheInterface {
+export interface cacheToSetInterface {
     key: string;
     val: any;
     durationInMinutes?: number;
+}
+
+export interface setCacheInterface {
+    data: cacheToSetInterface;
+    cachedData: cachedDataInterface[];
+    cacheTime?: number;
+    cacheLimit?: number;
+    cacheReorder?: boolean;
+}
+
+export interface getPartialCacheInterface {
+    partialKey: string | string[]; 
+    cachedData: cachedDataInterface[];
+    index?: number;
 }
